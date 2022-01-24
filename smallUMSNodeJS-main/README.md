123f3e8e0adbf2681d37f6ae1edceddaae79cc05
vaseemGit <vaseem.mohd@gmail.com>
2022-01-24 17:17:42 +0530

    Initial commit

---
+++ smallUMSNodeJS-main/README.md
@@ -0,0 +1,22 @@
+# small user management service NodeJS with - Express, Express-Handlebars, HBS, MySQL
+
+![GitHub Logo](https://github.com/mrigendrapratap/smallUMSNodeJS/blob/main/small%20user%20management.PNG)
+
+
+## Create .env file
+Create a .env file to store your database credentials
+
+```
+DB_HOST = localhost
+DB_NAME = smallusermanagement
+DB_USER = root
+DB_PASS = password
+```
+
+## Installation
+To run this project, install it locally using npm:
+
+```
+$ npm install
+$ npm start
+```
---
+++ smallUMSNodeJS-main/ufc-data.sql
@@ -0,0 +1,19 @@
+INSERT INTO `user` 
+(`id`, `first_name`,  `last_name`,    `email`,                 `phone`,         `comments`, `status`) VALUES
+(NULL, 'Mrigendra Pratap',      'Singh',        'cloud.mpsingh@gmail.com',        '7905121374', '',          'active'),
+(NULL, 'Pratap',      'Singh',        'cloudmpsingh@gmail.com',        '8726086068', '',          'active'),
+(NULL, 'Kamaru',      'Usman',        'kusman@abc.com',        '0123456789', '',          'active'),
+(NULL, 'Israel',      'Adesanya',     'iadesanya@abc.com',     '0123456789', '',          'active'),
+(NULL, 'Henry',       'Cejudo',       'hcejudo@abc.com',       '0123456789', '',          'active'),
+(NULL, 'Valentina',   'Shevchenko',   'vshevchenko@abc.com',   '0123456789', '',          'active'),
+(NULL, 'Tyron',       'Woodley',      'twoodley@abc.com',      '0123456789', '',          'active'),
+(NULL, 'Rose',        'Namajunas ',   'rnamajunas@abc.com',    '0123456789', '',          'active'),
+(NULL, 'Tony',        'Ferguson ',    'tferguson@abc.com',     '0123456789', '',          'active'),
+(NULL, 'Jorge',       'Masvidal ',    'jmasvidal@abc.com',     '0123456789', '',          'active'),
+(NULL, 'Nate',        'Diaz ',        'ndiaz@abc.com',         '0123456789', '',          'active'),
+(NULL, 'Conor',       'McGregor ',    'cmcGregor@abc.com',     '0123456789', '',          'active'),
+(NULL, 'Cris',        'Cyborg ',      'ccyborg@abc.com',       '0123456789', '',          'active'),
+(NULL, 'Tecia',       'Torres ',      'ttorres@abc.com',       '0123456789', '',          'active'),
+(NULL, 'Ronda',       'Rousey ',      'rrousey@abc.com',       '0123456789', '',          'active'),
+(NULL, 'Holly',       'Holm ',        'hholm@abc.com',         '0123456789', '',          'active'),
+(NULL, 'Joanna',      'Jedrzejczyk ', 'jjedrzejczyk@abc.com',  '0123456789', '',          'active')
